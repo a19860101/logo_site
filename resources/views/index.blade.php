c88463cfc2c2c01630595da1d8a4571a6d6c9e79
Zac <a19860101@gmail.com>
2020-10-15 17:24:52 +0800

    20201015

---
+++ resources/views/index.blade.php
@@ -0,0 +1,300 @@
+@extends('template.master')
+@section('main')
+
+<header>
+    <img src="https://picsum.photos/id/66/1800/600" class="w-100">
+</header>
+<div class="container py-5" id="feature">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="border-bottom pb-3 mb-4">Feature Logo</h2>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container py-5" id="new">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="border-bottom pb-3 mb-4">最新上架</h2>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 mb-4">
+            <div class="items position-relative bg-white rounded">
+                <a href="#">
+                    <img src="https://picsum.photos/id/15/800/600" class="w-100">
+                </a>
+                <div class="info border border-top-0 rounded p-3">
+                    <h4 class="title">logo 1</h4>
+                    <div class="price">$3,600</div>
+                    <hr>
+                    <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                </div>
+                <div class="actions position-absolute">
+                    <span class="favi"><i class="fas fa-heart fa-lg text-danger"></i></span>
+                    <span class="favi"><i class="far fa-heart fa-lg text-danger"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md align-self-center mb-4 mb-md-0">
+                <a href="#">
+                    <img src="https://picsum.photos/id/21/100" alt="">
+                </a>
+            </div>
+            <div class="col-md col-sm-6 mb-4 my-md-0">
+                <h4>關於品牌</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">品牌故事</a></li>
+                    <li><a href="#">服務條款</a></li>
+                    <li><a href="#">隱私條款</a></li>
+                </ul>
+            </div>
+            <div class="col-md col-sm-6 mb-4 my-md-0">
+                <h4>會員中心</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">會員註冊/登入</a></li>
+                    <li><a href="#">客制中心</a></li>
+                    <li><a href="#">會員條款</a></li>
+                </ul>
+            </div>
+            <div class="col-md col-sm-6 mb-4 my-md-0">
+                <h4>幫助中心</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">新手購物</a></li>
+                    <li><a href="#">常見問題</a></li>
+                    <li><a href="#">服務說明</a></li>
+                </ul>
+            </div>
+            <div class="col-md col-sm-6 mb-4 my-md-0">
+                <h4>聯絡我們</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">客服中心</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+@endsection
